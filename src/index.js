19071bfd37f4c051e77283244a36a23f26077845
Alma-GH <cuni.21@yandex.ru>
2022-07-18 23:45:06 +0300

    auth 1.5

--- src/index.js
+++ src/index.js
@@ -4,16 +4,37 @@ import {store} from "./store/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
-import ServerService from "./tools/Services/ServerService";
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getDatabase} from "firebase/database"
+import {DBContext} from "./context/DBContext";
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyg0IVxUt0Z6mmJsnJj-NUw5t0s3lOOQ0",
+  authDomain: "mylib-e19e9.firebaseapp.com",
+  databaseURL: "https://mylib-e19e9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mylib-e19e9",
+  storageBucket: "mylib-e19e9.appspot.com",
+  messagingSenderId: "68403622138",
+  appId: "1:68403622138:web:b585347d13b2bfc8de5943"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const db = getDatabase(app);
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <DBContext.Provider value={{auth,db}}>
       <BrowserRouter>
         <App/>
       </BrowserRouter>
+    </DBContext.Provider>
   </Provider>
 );
--- src/tools/Services/DatabaseAPI.js
+++ src/tools/Services/DatabaseAPI.js
@@ -1,90 +1,133 @@
 import {DB_FAV, DB_LISTS, DB_ROOT, DB_SUMM, DB_WORKS} from "../utils/const";
-
+import {child, get, ref, set} from "firebase/database";
 
 class DatabaseAPI{
 
+  _uid = null
+  _db = null
+
+  _folder = null
+
+  set user(val){
+    this._uid = val.uid
+    this._folder = `users/${this._uid}/`
+  }
+
+  set database(val){
+    this._db = val
+  }
+
 
   async getAllWorks(){
-    const res = await fetch(DB_ROOT + DB_WORKS)
-    return res.json()
+    // const res = await fetch(DB_ROOT + DB_WORKS)
+
+    // return res.json()
+
+
+    const res = await get(child(ref(this._db), this._folder + DB_WORKS))
+    console.log({res})
+    return res.val()
   }
 
   async setWorks(body){
-    const res = await fetch(DB_ROOT + DB_WORKS, {
-      method: "PUT",
-      headers:{
-        "Content-Type": "application/json"
-      },
-      body,
-    })
-    return res.json()
+    // const res = await fetch(DB_ROOT + DB_WORKS, {
+    //   method: "PUT",
+    //   headers:{
+    //     "Content-Type": "application/json"
+    //   },
+    //   body,
+    // })
+    // return res.json()
+
+
+    return set(ref(this._db, this._folder + DB_WORKS), body)
   }
 
 
   async getAllLists(){
-    const res = await fetch(DB_ROOT + DB_LISTS)
-    return res.json()
+    // const res = await fetch(DB_ROOT + DB_LISTS)
+    // return res.json()
+
+    const res = await get(child(ref(this._db), this._folder + DB_LISTS))
+    return res.val()
   }
 
   async setLists(body){
-    const res = await fetch(DB_ROOT + DB_LISTS, {
-      method: "PUT",
-      headers:{
-        "Content-Type": "application/json"
-      },
-      body,
-    })
-    return res.json()
+    // const res = await fetch(DB_ROOT + DB_LISTS, {
+    //   method: "PUT",
+    //   headers:{
+    //     "Content-Type": "application/json"
+    //   },
+    //   body,
+    // })
+    // return res.json()
+
+    return set(ref(this._db, this._folder + DB_LISTS), body)
   }
 
 
   async getAllFav(){
-    const res = await fetch(DB_ROOT + DB_FAV)
-    return res.json()
+    // const res = await fetch(DB_ROOT + DB_FAV)
+    // return res.json()
+
+    const res = await get(child(ref(this._db), this._folder + DB_FAV))
+    return res.val()
   }
 
   async setFav(body){
-    const res = await fetch(DB_ROOT + DB_FAV, {
-      method: "PUT",
-      headers:{
-        "Content-Type": "application/json"
-      },
-      body,
-    })
-    return res.json()
+    // const res = await fetch(DB_ROOT + DB_FAV, {
+    //   method: "PUT",
+    //   headers:{
+    //     "Content-Type": "application/json"
+    //   },
+    //   body,
+    // })
+    // return res.json()
+
+    return set(ref(this._db, this._folder + DB_FAV), body)
   }
 
 
   async getAllSummary(){
-    const res = await fetch(DB_ROOT + DB_SUMM)
-    return res.json()
+    // const res = await fetch(DB_ROOT + DB_SUMM)
+    // return res.json()
+
+    const res = await get(child(ref(this._db), this._folder + DB_SUMM))
+    return res.val()
   }
 
   async getSummaryById(id){
-    const res = await fetch(DB_ROOT + `/data/summary/${id}.json`)
-    return res.json()
+    // const res = await fetch(DB_ROOT + `/data/summary/${id}.json`)
+    // return res.json()
+
+    const res = await get(child(ref(this._db), this._folder + `/data/summary/${id}.json`))
+    return res.val()
   }
 
   async setSummaries(body){
-    const res = await fetch(DB_ROOT + DB_SUMM, {
-      method: "PUT",
-      headers:{
-        "Content-Type": "application/json"
-      },
-      body,
-    })
-    return res.json()
+    // const res = await fetch(DB_ROOT + DB_SUMM, {
+    //   method: "PUT",
+    //   headers:{
+    //     "Content-Type": "application/json"
+    //   },
+    //   body,
+    // })
+    // return res.json()
+
+    return set(ref(this._db, this._folder + DB_SUMM), body)
   }
 
   async setSummaryById(id, text){
-    const res = await fetch(DB_ROOT + `/data/summary/${id}.json`,{
-      method: "PUT",
-      headers:{
-        "Content-Type": "application/json"
-      },
-      body: text,
-    })
-    return res.json()
+    // const res = await fetch(DB_ROOT + `/data/summary/${id}.json`,{
+    //   method: "PUT",
+    //   headers:{
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: text,
+    // })
+    // return res.json()
+
+    return set(ref(this._db, this._folder + `/data/summary/${id}.json`), text)
   }
 
 }
